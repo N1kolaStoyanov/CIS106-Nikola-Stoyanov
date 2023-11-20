de00d2e1ecada588050c41a1126d1d31fcd53e19
Nikola Stoyanov <110687131+N1kolaStoyanov@users.noreply.github.com>
2023-11-19 18:08:20 -0600

    Updated PS9P2.py
    added name inpout

--- Assignment  9/PS9P2.py
+++ Assignment  9/PS9P2.py
@@ -6,9 +6,10 @@ r = input("Do you wish to calculate batting avarage (y/n)?")
 
 p = 0
 while r == "y":
+  name = input("Enter Last Name")
   hits = float(input("Please Enter Number of Hits"))
   bats = float(input("Please Enter Number of Bats taken"))
   p = p +1
-  print("Batting Average is ", battingavg(hits, bats))
+  print(name ,"'s" ,"batting Average is ", battingavg(hits, bats))
   r = input("Do you wish to calculate batting avarage (y/n)?")
 print("You have calculated ", p, " batting averages")
